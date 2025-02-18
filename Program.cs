4b1d09f43ff32d86ac37d9378a62f2579ee08f74
Krishna Murali Andiboyina <murali.andiboyina@evolutyz.com>
2025-02-18 13:04:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using DSA;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 8, 32, 14, 1, 78, 40 };
+
+            //SortingTechniques.SelectionSort(arr);
+            SortingTechniques.BubbleSort(arr);
+        }
+
+
+    }
+}
---
+++ SortingTechniques.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    public class SortingTechniques
+    {
+        public static void SelectionSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[min] > arr[j])
+                    {
+                        min = j;
+                    }
+                }
+
+                SortingTechniques.Swap(arr, min, i);
+            }
+
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        public static void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                bool isSwapped = false;
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        SortingTechniques.Swap(arr, i, j); ;
+                        isSwapped = true;
+                    }
+                }
+
+                if (!isSwapped)
+                {
+                    break;
+                }
+            }
+
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        public static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
